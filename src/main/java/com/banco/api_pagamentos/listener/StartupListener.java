a14dbeb6e3448011a226183035ddf0a249c392d8
hudsonpedroso <hudsonrodrigo.junior@autbank.com.br>
2024-11-27 18:17:11 -0300

    improve: adição do link do banco de dados no log de inicio do console na classe StartupListener

--- src/main/java/com/banco/api_pagamentos/listener/StartupListener.java
+++ src/main/java/com/banco/api_pagamentos/listener/StartupListener.java
@@ -15,5 +15,10 @@ public class StartupListener implements ApplicationListener<ApplicationReadyEven
         log.info("============================== A aplicação foi iniciada! =================================");
         log.info("========= Acesse e teste a API no Swagger UI: http://localhost:8080/swagger-ui.html ======");
         log.info("==========================================================================================");
+        log.info("=========================== O Banco de dados H2 foi iniciado! ============================");
+        log.info("====================================== Usuario: sa =======================================");
+        log.info("====================================== Senha: (vazio) ====================================");
+        log.info("=========== Acesse e verifique os dados: http://localhost:8080/h2-console ================");
+        log.info("==========================================================================================");
     }
 }
